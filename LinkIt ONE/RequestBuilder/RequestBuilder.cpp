c193df5d841e627fbc8e82512e821f53db139785
Fury-toC <60314520+Fury-toC@users.noreply.github.com>
2020-12-04 16:37:55 +0200

    Minor changes in RequestBuilder

--- LinkIt ONE/RequestBuilder/RequestBuilder.cpp
+++ LinkIt ONE/RequestBuilder/RequestBuilder.cpp
@@ -4,40 +4,22 @@
 
 using namespace std;
 
-
-array<string, 4> request_builder (string data, string method, string url, string conection_type) {
+array<string, 4> request_builder (string method, string url, string connection_type, string data) {
     array<string, 4> container;
-    int i = 0;
-    container[i] = std::move(data);
-    i++;
-    container[i] = std::move(method);
-    i++;
-    container[i] = std::move(url);
-    i++;
-    container[i] = std::move(conection_type);
+    container[0] = std::move(method) + " / HTTP/1.1";
+    container[1] = "Host: " + std::move(url);
+    container[2] = "Connection: " + std::move(connection_type);
+    container[3] = std::move(data);
     return container;
 }
 
-void data_out(string data, string method, string url, string conection_type, short style) {
-    array<string, 4> after  = request_builder(std::move(data), std::move(method), std::move(url), std::move(conection_type));
-    if (style == 1)
-        for (int i = 0; i < 4; i++)
-            cout << after[i] + "\n";
-    else
-        for (int i = 0; i < 4; i++)
-            cout << after[i] + "\t\t";
-
-}
-
-
 int main() {
     array<string, 4> result;
-    string dat = "\"data variable\"";
-    string metho = "\"method of ...\"";
-    string ur = "\"your favorite website\"";
-    string con = "\"con_type 10\"";
+    string dat = "7685658578.07; 45749090.00";
+    string meth = "Post";
+    string ur = "www.wikipedia.com";
+    string conn = "keep alive";
     short type;
-    //enter type of output
-    cin >> type; //all numbers without "1" will output array by \n, else it will be otputed by \t 
-    data_out(dat, metho, ur, con, type);
+    cin >> type;
+    result = request_builder(meth, ur, conn, dat);
 }

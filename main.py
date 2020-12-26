d7960abae49c8962368580d6bec831562532796d
Fury-toC <60314520+Fury-toC@users.noreply.github.com>
2020-12-26 20:49:31 +0200

    Move all data from Linux
    Just all stuff and even drafts in special folder like "old_stuff"

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/home")
+def main_p():
+    return render_template("homePage.html")
+
+
+@app.route("/about")
+def about_p():
+    return render_template("aboutPage.html")
+
+
+@app.route("/contacts")
+def message_p():
+    return render_template("messageForm.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
